d0bbe592c216ae7af56afa18b45bea0f6f788c44
cburakasan <64010299+cburakasan@users.noreply.github.com>
2020-12-13 00:42:47 +0300

    --add test images from postman

--- src/main/java/com/asan/todosecurity/User/Dto/UserResponse.java
+++ src/main/java/com/asan/todosecurity/User/Dto/UserResponse.java
@@ -1,43 +1,13 @@
 package com.asan.todosecurity.User.Dto;
 
+import lombok.Data;
+
 import java.util.List;
 
+@Data
 public class UserResponse
 {
-    String userName;
-    String token;
-    List<String> roleList;
-    boolean admin;
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public List<String> getRoleList() {
-        return roleList;
-    }
-
-    public void setRoleList(List<String> roleList) {
-        this.roleList = roleList;
-    }
-
-    public boolean isAdmin() {
-        return admin;
-    }
-
-    public void setAdmin(boolean admin) {
-        this.admin = admin;
-    }
+    private String token;
+    private List<String> roleList;
+    private boolean admin;
 }
